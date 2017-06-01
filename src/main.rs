526d7674e806381a11f6ed64a738d73fae89a089
Armin Häberling <armin.aha@gmail.com>
2017-06-01 21:38:28 +0200

    Simple HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate reqwest;
+
+use reqwest::IntoUrl;
+
+use std::env;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let url = env::args().nth(1).expect("url");
+    if let Err(e) = do_request(&url) {
+        println!("Error: {}", e.description());
+        println!("{:?}", e);
+    }
+}
+
+fn do_request<T: IntoUrl>(url: T) -> Result<(), Box<std::error::Error>> {
+    let mut resp = reqwest::get(url)?;
+    let mut content = String::new();
+    resp.read_to_string(&mut content)?;
+    println!("{}", content);
+    Ok(())
 }
