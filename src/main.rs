200937cb2471a05595efea270e76cfa8da6fd95c
Armin Häberling <armin.aha@gmail.com>
2017-06-01 23:26:39 +0200

    Parse to enum values

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,63 @@ struct JobList {
 #[derive(Serialize, Deserialize, Debug)]
 struct Job {
     name: String,
-    color: String,
+    color: Color,
     #[serde(rename = "lastBuild")]
     last_build: Option<Build>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+enum Color {
+    #[serde(rename = "red")]
+    Red,
+    #[serde(rename = "red_anime")]
+    RedAnime,
+    #[serde(rename = "yellow")]
+    Yellow,
+    #[serde(rename = "yellow_anime")]
+    YellowAnime,
+    #[serde(rename = "blue")]
+    Blue,
+    #[serde(rename = "blue_anime")]
+    BlueAnime,
+    // for historical reasons they are called grey.
+    #[serde(rename = "grey")]
+    Grey,
+    #[serde(rename = "grey_anime")]
+    GreyAnime,
+    #[serde(rename = "disabled")]
+    Disabled,
+    #[serde(rename = "disabled_anime")]
+    DisabledAnime,
+    #[serde(rename = "aborted")]
+    Aborted,
+    #[serde(rename = "aborted_anime")]
+    AbortedAnime,
+    #[serde(rename = "notbuilt")]
+    NotBuilt,
+    #[serde(rename = "notbuilt_anime")]
+    NotBuiltAnime,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Build {
     number: u32,
-    result: String,
+    result: BuildResult,
     timestamp: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum BuildResult {
+    #[serde(rename = "SUCCESS")]
+    Success,
+    #[serde(rename = "UNSTABLE")]
+    Unstable,
+    #[serde(rename = "FAILURE")]
+    Failure,
+    #[serde(rename = "NOT_BUILT")]
+    NotBuilt,
+    #[serde(rename = "ABORTED")]
+    Aborted,
 }
 
 fn retrieve_jobs<T: IntoUrl>(jenkins_url: T) -> Result<Vec<Job>, Box<std::error::Error>> {
