82c30e543f0e9493d9c5283752ea9b39be5f55c2
Armin Häberling <armin.aha@gmail.com>
2017-06-01 22:01:52 +0200

    Retrieve jenkins jobs

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ use std::env;
 use std::io::Read;
 
 fn main() {
-    let url = env::args().nth(1).expect("url");
-    if let Err(e) = do_request(&url) {
+    let jenkins_url = env::args().nth(1).expect("jenkins url");
+    if let Err(e) = do_request(&jenkins_url) {
         println!("Error: {}", e.description());
         println!("{:?}", e);
     }
 }
 
-fn do_request<T: IntoUrl>(url: T) -> Result<(), Box<std::error::Error>> {
+fn do_request<T: IntoUrl>(jenkins_url: T) -> Result<(), Box<std::error::Error>> {
+    let url = jenkins_url
+        .into_url()?
+        .join("api/json?tree=jobs[name,color]")?;
     let mut resp = reqwest::get(url)?;
     let mut content = String::new();
     resp.read_to_string(&mut content)?;
