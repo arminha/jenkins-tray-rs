36f39dcd0e07745ec4a722f0ded824fe0391fcac
Armin Häberling <armin.aha@gmail.com>
2017-06-05 21:45:37 +0200

    Update tray icon with jenkins status

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod tray;
 
 use reqwest::IntoUrl;
 
+use jenkins::JenkinsStatus;
 use tray::{Tray, TrayStatus};
 
 use std::cell::RefCell;
@@ -31,22 +32,21 @@ fn main() {
     let mut tray = tray_cell.borrow_mut();
 
     {
-        let tray_cell = tray_cell.clone();
-        tray.add_menu_item("Change icon", None, move || {
-            let mut tray = tray_cell.borrow_mut();
-            tray.set_status(TrayStatus::Success);
-        });
-    }
-    {
-        let jenkins_url = jenkins_url.clone();
-        tray.add_menu_item("Print Jobs", None, move || print_jobs(&jenkins_url));
-    }
-    {
         let jenkins_url = jenkins_url.clone();
         tray.add_menu_item("Open Jenkins",
                            None,
                            move || if open::that(&jenkins_url).is_err() {
                                println!("Failed to open Jenkins");
+                           });
+    }
+    {
+        let jenkins_url = jenkins_url.clone();
+        let tray_cell = tray_cell.clone();
+        tray.add_menu_item("Update",
+                           None,
+                           move || if let Some(status) = retrieve_tray_status(&jenkins_url) {
+                               let mut tray = tray_cell.borrow_mut();
+                               tray.set_status(status);
                            });
     }
     tray.add_menu_item("Exit", Some("application-exit"), || gtk::main_quit());
@@ -57,15 +57,23 @@ fn main() {
     gtk::main();
 }
 
-fn print_jobs<T: IntoUrl>(jenkins_url: T) {
+fn retrieve_tray_status<T: IntoUrl>(jenkins_url: T) -> Option<TrayStatus> {
     match jenkins::retrieve_jobs(jenkins_url) {
         Err(e) => {
             println!("Error: {}", e.description());
             println!("{:?}", e);
+            None
         }
         Ok(jobs) => {
             let status = jenkins::aggregate_status(jobs);
-            println!("{:?}", status);
+            let tray_status = match status {
+                JenkinsStatus::Success => TrayStatus::Success,
+                JenkinsStatus::Unstable(_) => TrayStatus::Unstable,
+                JenkinsStatus::Failure(_) => TrayStatus::Failure,
+                JenkinsStatus::Unknown => TrayStatus::Unknown,
+                JenkinsStatus::NotBuilt => TrayStatus::NotBuilt,
+            };
+            Some(tray_status)
         }
     }
 }
