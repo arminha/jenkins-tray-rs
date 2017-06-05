0e9107ecf05bacfde6ddb31e507169a586a31681
Armin Häberling <armin.aha@gmail.com>
2017-06-05 16:15:25 +0200

    Move tray methods to tray module

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,15 @@ extern crate libappindicator;
 extern crate open;
 
 mod jenkins;
+mod tray;
 
 use reqwest::IntoUrl;
 
-use gtk::{WidgetExt, MenuShellExt, MenuItemExt, ContainerExt};
-use libappindicator::{AppIndicator, AppIndicatorStatus};
+use tray::{Tray, TrayStatus};
 
+use std::cell::RefCell;
 use std::env;
-use std::sync::{Arc, Mutex};
+use std::rc::Rc;
 
 fn main() {
     let jenkins_url = env::args().nth(1).expect("jenkins url");
@@ -26,65 +27,34 @@ fn main() {
         return;
     }
 
-    let assets = find_folder::Search::ParentsThenKids(3, 3)
-        .for_folder("assets")
-        .unwrap();
-    let blue = assets.join("blue.png");
-    let yellow = assets.join("yellow.png");
+    let tray_cell = Rc::new(RefCell::new(Tray::new()));
+    let mut tray = tray_cell.borrow_mut();
 
-    let mut indicator = AppIndicator::new("libappindicator test application", "");
-    indicator.set_status(AppIndicatorStatus::APP_INDICATOR_STATUS_ACTIVE);
-    indicator.set_icon_full(blue.to_str().unwrap(), "blue");
-    let mut m = gtk::Menu::new();
-
-    let mutex = Arc::new(Mutex::new(indicator));
-
-    let mi = create_menu_item("Exit", Some("application-exit"));
-    mi.connect_activate(|_| gtk::main_quit());
-
-    let mi2 = create_menu_item("Change icon", None);
-    let mutex2 = mutex.clone();
-    mi2.connect_activate(move |_| {
-        let mut indicator = mutex2.lock().unwrap();
-        indicator.set_icon_full(yellow.to_str().unwrap(), "yellow");
-    });
-
-    let mi3 = create_menu_item("Print Jobs", None);
+    {
+        let tray_cell = tray_cell.clone();
+        tray.add_menu_item("Change icon", None, move |_| {
+            let mut tray = tray_cell.borrow_mut();
+            tray.set_status(TrayStatus::Success);
+        });
+    }
     {
         let jenkins_url = jenkins_url.clone();
-        mi3.connect_activate(move |_| print_jobs(&jenkins_url));
+        tray.add_menu_item("Print Jobs", None, move |_| print_jobs(&jenkins_url));
     }
+    {
+        let jenkins_url = jenkins_url.clone();
+        tray.add_menu_item("Open Jenkins",
+                           None,
+                           move |_| if open::that(&jenkins_url).is_err() {
+                               println!("Failed to open Jenkins");
+                           });
+    }
+    tray.add_menu_item("Exit", Some("application-exit"), |_| gtk::main_quit());
 
-    let mi4 = create_menu_item("Open Jenkins", None);
-    mi4.connect_activate(move |_| {
-        if open::that(&jenkins_url).is_err() {
-            println!("Failed to open Jenkins");
-        }
-    });
+    tray.show_all();
+    std::mem::drop(tray);
 
-    m.append(&mi2);
-    m.append(&mi3);
-    m.append(&mi4);
-    m.append(&mi);
-
-    mutex.lock().unwrap().set_menu(&mut m);
-    m.show_all();
     gtk::main();
-}
-
-fn create_menu_item(label: &str, icon_name: Option<&str>) -> gtk::MenuItem {
-    if let Some(icon_name) = icon_name {
-        let icon = gtk::Image::new_from_icon_name(icon_name, gtk::IconSize::Menu.into());
-        let lbl = gtk::Label::new(label);
-        let b = gtk::Box::new(gtk::Orientation::Horizontal, 6);
-        b.add(&icon);
-        b.add(&lbl);
-        let mi = gtk::MenuItem::new();
-        mi.add(&b);
-        mi
-    } else {
-        gtk::MenuItem::new_with_label(label)
-    }
 }
 
 fn print_jobs<T: IntoUrl>(jenkins_url: T) {
---
+++ src/tray.rs
@@ -0,0 +1,93 @@
+use find_folder;
+use gtk::{Box, ContainerExt, IconSize, Image, Label, Menu, MenuItem, MenuItemExt, MenuShellExt,
+          Orientation, WidgetExt};
+use libappindicator::{AppIndicator, AppIndicatorStatus};
+
+pub enum TrayStatus {
+    Unknown,
+    Success,
+    Unstable,
+    Failure,
+    NotBuilt,
+    Disabled,
+}
+
+impl TrayStatus {
+    fn icon_name(&self) -> String {
+        let assets = find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets")
+            .unwrap();
+        let file = match *self {
+            TrayStatus::Unknown => "grey.png",
+            TrayStatus::Success => "blue.png",
+            TrayStatus::Unstable => "yellow.png",
+            TrayStatus::Failure => "red.png",
+            TrayStatus::NotBuilt => "grey.png",
+            TrayStatus::Disabled => "grey.png",
+        };
+        assets.join(file).to_string_lossy().into_owned()
+    }
+
+    fn desc(&self) -> &str {
+        match *self {
+            TrayStatus::Unknown => "Unknown",
+            TrayStatus::Success => "Success",
+            TrayStatus::Unstable => "Unstable",
+            TrayStatus::Failure => "Failure",
+            TrayStatus::NotBuilt => "NotBuilt",
+            TrayStatus::Disabled => "Disabled",
+        }
+    }
+}
+
+pub struct Tray {
+    indicator: AppIndicator,
+    menu: Menu,
+}
+
+fn create_menu_item(label: &str, icon_name: Option<&str>) -> MenuItem {
+    if let Some(icon_name) = icon_name {
+        let icon = Image::new_from_icon_name(icon_name, IconSize::Menu.into());
+        let lbl = Label::new(label);
+        let b = Box::new(Orientation::Horizontal, 6);
+        b.add(&icon);
+        b.add(&lbl);
+        let mi = MenuItem::new();
+        mi.add(&b);
+        mi
+    } else {
+        MenuItem::new_with_label(label)
+    }
+}
+
+impl Tray {
+    pub fn new() -> Tray {
+        let mut indicator = AppIndicator::new("Jenkins Tray", "");
+        indicator.set_status(AppIndicatorStatus::APP_INDICATOR_STATUS_ACTIVE);
+        let status = TrayStatus::Unknown;
+        indicator.set_icon_full(&status.icon_name(), status.desc());
+
+        let mut menu = Menu::new();
+        indicator.set_menu(&mut menu);
+
+        Tray { indicator, menu }
+    }
+
+    pub fn set_status(&mut self, status: TrayStatus) {
+        self.indicator
+            .set_icon_full(&status.icon_name(), status.desc());
+    }
+
+    pub fn add_menu_item<F: Fn(&MenuItem) + 'static>(&mut self,
+                                                     label: &str,
+                                                     icon_name: Option<&str>,
+                                                     callback: F) {
+        let item = create_menu_item(label, icon_name);
+        item.connect_activate(callback);
+        self.menu.append(&item);
+    }
+
+    pub fn show_all(&mut self) {
+        self.menu.show_all();
+    }
+}
