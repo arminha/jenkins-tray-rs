0de8ffe411d07c691529366e44b1ff418691ed25
Armin Häberling <armin.aha@gmail.com>
2018-10-18 21:15:38 +0200

    Format

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #[forbid(unsafe_code)]
-
 extern crate reqwest;
 #[macro_use]
 extern crate serde_derive;
